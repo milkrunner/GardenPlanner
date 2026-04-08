5ef23d26555db29e0be4475ad2fe754237ad647b
Niklas Horstmann <niklas.horstmann@inform-software.com>
2026-04-08 11:27:31 +0200

    feat(pwa): Meta-Tags und SW-Registration in alle Seiten (#46)

---
+++ src/js/pwa-register.js
@@ -0,0 +1,22 @@
+// PWA Service Worker Registration
+(function() {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+            navigator.serviceWorker.register('/sw.js')
+                .then(function(registration) {
+                    registration.addEventListener('updatefound', function() {
+                        var newWorker = registration.installing;
+                        newWorker.addEventListener('statechange', function() {
+                            if (newWorker.state === 'activated') {
+                                console.log('[PWA] Neuer Service Worker aktiviert');
+                            }
+                        });
+                    });
+                    console.log('[PWA] Service Worker registriert:', registration.scope);
+                })
+                .catch(function(error) {
+                    console.warn('[PWA] Service Worker Registration fehlgeschlagen:', error);
+                });
+        });
+    }
+})();
